36b5360fee0e5db962669707071e93ac259032d0
jinghun1999 <893884166@qq.com>
2017-11-17 18:36:45 +0800

    修改首页渲染逻辑，使用整个FlatList进行渲染

--- src/pages/bohai/BHStart.js
+++ src/pages/bohai/BHStart.js
@@ -2,13 +2,19 @@
  * Created by TomChow on 17/11/16.
  */
 import React, {Component} from 'react'
-import {View } from 'react-native'
+import {
+    ScrollView,
+    View,
+    Text,
+    StyleSheet,
+    Image
+} from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Container, Header, Content, Button, Text } from 'native-base';
+import { Container, Header, Content, Button } from 'native-base';
 import {observer, inject} from 'mobx-react/native'
 
 @observer
-export default class Start extends Component {
+export default class BHStart extends Component {
     constructor(props){
         super(props);
     }
@@ -19,29 +25,38 @@ export default class Start extends Component {
 
     render() {
         return (
-                <Content>
-                    <Button block light>
-                        <Text>Light</Text>
-                    </Button>
-                    <Button block>
-                        <Text>Primary</Text>
-                    </Button>
-                    <Button block success>
-                        <Text>Success</Text>
-                    </Button>
-                    <Button block info>
-                        <Text>Info</Text>
-                    </Button>
-                    <Button block warning>
-                        <Text>Warning</Text>
-                    </Button>
-                    <Button block danger>
-                        <Text>Danger</Text>
-                    </Button>
-                    <Button block dark>
-                        <Text>Dark</Text>
-                    </Button>
-                </Content>
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.topBox}>
+                        <View style={styles.topBoxLine}>
+                            <Image source={require('../../resource/bohai_logo.png')} style={{width: 40, height: 40}}/>
+                            <Text>众联检测服务</Text>
+                            <Button>检测说明</Button>
+                        </View>
+                    </View>
+
+                </View>
+            </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    topBox:{
+        marginTop:5,
+        padding:15,
+        borderTopWidth:3,
+        borderTopColor:'#ff9800',
+        backgroundColor:'#fff',
+
+    },
+    topBoxLine:{
+        paddingBottom:3,
+        borderBottomWidth:1,
+        borderBottomColor:'#ccc',
+        flexDirection:'row',
+    }
+})
