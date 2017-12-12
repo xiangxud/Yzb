e281a0d77df5af9aa92b7b5a334f440658f7bc3d
jinghun1999 <893884166@qq.com>
2017-12-12 16:30:14 +0800

    sty homepage, monitor

--- src/store/styStore.js
+++ src/store/styStore.js
@@ -1,11 +1,8 @@
 import {AsyncStorage} from 'react-native'
 import {action, computed, observable, reaction, runInAction, useStrict} from 'mobx'
-import { persist } from 'mobx-persist'
-import _ from "lodash";
 useStrict(true);
 
-class ImmCollectionStore
-{
+class ImmCollectionStore {
     constructor(){
     }
     @observable
