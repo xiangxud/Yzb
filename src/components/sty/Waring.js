983a7539b0f4184093851024933b316d30f106a4
jinghun1999 <893884166@qq.com>
2017-12-12 20:46:46 +0800

    sty...camera.change...imm remind.

--- src/components/sty/Waring.js
+++ src/components/sty/Waring.js
@@ -18,62 +18,67 @@ export default class Waring extends Component{
     }
     empty = (e) =>{
         return !e || e==null || e=="";
-    };
+    }
     humidityWaring = (humidity)=>{
-        if( !this.empty(humidity) ){
-            return (<View style={style.item}><Text>湿度高</Text><Text style={style.red}>{humidity}</Text></View>);
+        if(!this.empty(humidity)){
+            return (
+                <View style={style.item}><Text>湿度</Text><Text style={style.red}>{humidity}</Text></View>
+            )
         }
-    };
+    }
     temperatureWaring = (temperature)=>{
-        if( !this.empty(temperature) ){
-            return (<View style={style.item}><Text>温度高</Text><Text style={style.red}>{temperature}</Text></View>);
+        if(!this.empty(temperature)){
+            return (
+                <View style={style.item}><Text>温度</Text><Text style={style.red}>{temperature}</Text></View>
+            )
         }
-    };
+    }
     co2Waring = (co2)=>{
-        if( !this.empty(co2) ){
-            return (<View style={style.item}><Text>二氧化碳</Text><Text style={style.red}>{co2}</Text></View>);
+        if(!this.empty(co2)){
+            return (
+                <View style={style.item}><Text>二氧化碳</Text><Text style={style.red}>{co2}</Text></View>
+            )
         }
-    };
+    }
     otherWaring =(humidity,temperature,co2)=>{
         if(this.empty(humidity) && this.empty(temperature) && this.empty(co2)){
             return (
-                <Text>温度、湿度、二氧化碳均未见异常</Text>
-            );
+                <Text>温度、湿度、二氧化碳均正常</Text>
+            )
         }else if(this.empty(temperature) && this.empty(humidity) ){
             return (
-                <Text>温度、湿度未见异常</Text>
-            );
+                <Text>温度、湿度正常</Text>
+            )
         }else if(this.empty(humidity) && this.empty(co2) ){
             return (
-                <Text>湿度、二氧化碳未见异常</Text>
-            );
+                <Text>湿度、二氧化碳正常</Text>
+            )
         }else if(this.empty(temperature) && this.empty(co2) ){
             return (
-                <Text>温度、二氧化碳未见异常</Text>
-            );
+                <Text>温度、二氧化碳正常</Text>
+            )
         }else if(this.empty(humidity) ){
             return (
-                <Text>湿度未见异常</Text>
-            );
+                <Text>湿度正常</Text>
+            )
         }else if(this.empty(temperature) ){
             return (
-                <Text>温度未见异常</Text>
-            );
+                <Text>温度正常</Text>
+            )
         }else if(this.empty(co2) ){
             return (
-                <Text>二氧化碳未见异常</Text>
-            );
+                <Text>二氧化碳正常</Text>
+            )
         }
-    };
+    }
 
     render(){
         const{ waring }= this.props;
         return <View style={style.container}>
-            <Text>{waring.genus}{waring.count},</Text>
             {this.humidityWaring(waring.humidity)}
             {this.temperatureWaring(waring.temperature)}
             {this.co2Waring(waring.co2)}
-            {this.otherWaring(waring.humidity,waring.temperature,waring.co2)}
+            {this.otherWaring(waring.humidity, waring.temperature, waring.co2)}
         </View>
     }
 };
